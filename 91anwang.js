2d04518a9cd8de74d3368e2960995e1d31d245aa
daboluohuozhe <155328781+daboluohuozhe@users.noreply.github.com>
2024-06-29 07:44:09 +0800

    91anwang.js

--- 91anwang.js
+++ 91anwang.js
@@ -3,7 +3,7 @@
 
 [rewrite_local]
 
-https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
+https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https://raw.githubusercontent.com/daboluohuozhe/DABOLUO/main/91.js
 
 [mitm]
 hostname = *.cloudfront.net, *.ninghaixin.club
