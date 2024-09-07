0aa6543980887fb863c855b3baa0b177bc18cef6
daboluohuozhe <155328781+daboluohuozhe@users.noreply.github.com>
2024-09-07 14:07:04 +0800

    91kai.js

--- 91kai.js
+++ 91kai.js
@@ -1,8 +1,8 @@
-
+/*
 [rewrite_local]
 
 https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https://raw.githubusercontent.com/daboluohuozhe/DABOLUO/main/91wo.js
 
 [mitm]
 hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip, %APPEND% *play.*.cn,long.*.cn
-
+*/
