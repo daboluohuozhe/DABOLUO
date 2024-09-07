7d757f747ef05852dfe70d9d11975c2bf7054646
daboluohuozhe <155328781+daboluohuozhe@users.noreply.github.com>
2024-09-07 14:20:35 +0800

    91kai.js

--- 91kai.js
+++ 91kai.js
@@ -9,4 +9,4 @@ hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d1
 [MITM]
 hostname =  %APPEND% *play.*.cn,long.*.cn,*.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip,
 [Script]
-瑟瑟视频 = type=http-request,pattern=^https:\/\/[^\/]*\..*\.cn\/\w+\/[a-z0-9]{32}\/[a-z0-9]{32}\.m3u8(\?.*)?,requires-body=0,script-path=https://raw.githubusercontent.com/daboluohuozhe/DABOLUO/main/91wo.js,script-update-interval=0
+瑟瑟视频 = type=http-request,pattern=^https:\/\/[^\/]*\..*\.cn\/\w+\/[a-z0-9]{32}\/[a-z0-9]{32}\.m3u8(\?.*)?,requires-body=0,script-path=https://raw.githubusercontent.com/daboluohuozhe/DABOLUO/main/TONGSHA.js,script-update-interval=0
