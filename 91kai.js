31a46a6c3b03700beda5f46c52f9e3c7bfb01729
daboluohuozhe <155328781+daboluohuozhe@users.noreply.github.com>
2024-09-07 14:13:17 +0800

    91kai.js

--- 91kai.js
+++ 91kai.js
@@ -6,3 +6,7 @@ https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https
 [mitm]
 hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip, %APPEND% *play.*.cn,long.*.cn
 */
+[MITM]
+hostname =  %APPEND% *play.*.cn,long.*.cn,*.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip,
+[Script]
+瑟瑟视频 = type=http-request,pattern=^https:\/\/[^\/]*\..*\.cn\/\w+\/[a-z0-9]{32}\/[a-z0-9]{32}\.m3u8(\?.*)?,requires-body=0,script-path=https://raw.githubusercontent.com/Alex0510/Eric/master/surge/Script/tangtoutiao.js,script-update-interval=0
