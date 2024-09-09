3bc7b8bd6bd5edc1ac96ae3540f870b528d5b22c
daboluohuozhe <155328781+daboluohuozhe@users.noreply.github.com>
2024-09-09 16:44:57 +0800

    91kai.js

--- 91kai.js
+++ 91kai.js
@@ -7,6 +7,6 @@ https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https
 hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip, %APPEND% *play.*.cn,long.*.cn
 */
 [MITM]
-hostname =  %APPEND% *play.*.cn,long.*.cn,*.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip,
+hostname =  %APPEND% *play.*.cn,long.*.cn,*.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip,*.tuplrf.cn
 [Script]
 瑟瑟视频 = type=http-request,pattern=^https:\/\/[^\/]*\..*\.cn\/\w+\/[a-z0-9]{32}\/[a-z0-9]{32}\.m3u8(\?.*)?,requires-body=0,script-path=https://raw.githubusercontent.com/daboluohuozhe/DABOLUO/main/TONGSHA.js,script-update-interval=0
