d2e1fc9f15f6c7ae5acc25a49e00a3c3214551ee
daboluohuozhe <155328781+daboluohuozhe@users.noreply.github.com>
2025-09-18 11:19:23 +0800

    Add files via upload

--- TONGSHA2.js
+++ TONGSHA2.js
@@ -31,7 +31,7 @@ if (!playerScheme) {
 }
 
 // 构造跳转 URL，确保 URL 被正确编码
-const fullURL = playerScheme + encodeURIComponent(url);
+const fullURL = playerMap + encodeURIComponent(url);
 
 // 发送通知并跳转到 nPlayer 播放器
 $.msg("打开播放器", fullURL, "", fullURL);
