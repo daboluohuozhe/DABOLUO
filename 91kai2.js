49231724a1a311cd17d06e4509947968b11bf7cc
daboluohuozhe <155328781+daboluohuozhe@users.noreply.github.com>
2025-10-02 00:11:56 +0800

    Add files via upload

--- 91kai2.js
+++ 91kai2.js
@@ -1,12 +1,3 @@
-/*
 [rewrite_local]
-
-https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https://raw.githubusercontent.com/daboluohuozhe/DABOLUO/main/91wo.js
-
-[mitm]
-hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip, %APPEND% *play.*.cn,long.*.cn
-*/
-[MITM]
-hostname =  %APPEND% *play.*.cn,long.*.cn,*.zhuoxianyx.xyz,tt2340sdf.com,*.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip,*.tuplrf.cn
-[Script]
-瑟瑟视频 = type=http-request,pattern=^https:\/\/[^\/]*\..*\.cn\/\w+\/[a-z0-9]{32}\/[a-z0-9]{32}\.m3u8(\?.*)?,requires-body=0,script-path=https://raw.githubusercontent.com/daboluohuozhe/DABOLUO/refs/heads/main/TONGSHA2.js,script-update-interval=0
+# 拦截所有m3u8链接并发送通知
+^https?:\/\/[^\/]+\.m3u8.*$ url script-response-body https://raw.githubusercontent.com/daboluohuozhe/DABOLUO/refs/heads/main/TONGSHA2.js
