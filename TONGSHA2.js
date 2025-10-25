5018a1b4741621f592422ca87c949b6396d6c644
daboluohuozhe <155328781+daboluohuozhe@users.noreply.github.com>
2025-10-26 01:37:57 +0800

    Add files via upload

--- TONGSHA2.js
+++ TONGSHA2.js
@@ -1,7 +1,6 @@
 
-const $ = new Env("开始喽"+url);
 let url = $request.url, headers = $request.headers;
-const $ = new Env(url);
+const $ = new Env("开始喽"+url);
 // X-Playback-Session-Id头部
 if (headers.hasOwnProperty("X-Playback-Session-Id")) {
     $.msg("成功", "点击复制链接", "", {
