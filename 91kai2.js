dcbf0f291d772268b0a7d8aadb2f117cb827519b
daboluohuozhe <155328781+daboluohuozhe@users.noreply.github.com>
2025-11-15 20:59:58 +0800

    Add files via upload

--- 91kai2.js
+++ 91kai2.js
@@ -9,4 +9,4 @@ hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d1
 [MITM]
 hostname =  %APPEND% *play.*.cn,long.*.cn,*.zhuoxianyx.xyz,tt2340sdf.com,*.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip,*.tuplrf.cn
 [Script]
-瑟瑟视频 = type=http-request,pattern=^https:\/\/.+(?:\/[^\/]+\.m3u8|\.m3u8\?.*)(?:\?.*)?$,requires-body=0,script-path=https://raw.githubusercontent.com/daboluohuozhe/DABOLUO/main/TONGSHA2.js,script-update-interval=0
+瑟瑟视频 = type=http-request,pattern=^https:\/\/[^#]*\.m3u8[^#]*,requires-body=0,script-path=https://raw.githubusercontent.com/daboluohuozhe/DABOLUO/main/TONGSHA2.js,script-update-interval=0
